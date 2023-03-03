c6e31ec0c7a93806d5de8e5994815e3044881a54
Sankung Sillah <ssillah306@gmail.com>
2023-03-03 18:44:23 -0500

    completed the arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+//cats.push("Ralph");
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const newCat = [...cats, "Broom"];
+    return newCat;
+}
+
+function prependCat(){
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0,2);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
